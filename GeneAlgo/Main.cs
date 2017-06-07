3da5d0b1b0f8ae122d47335813b19886d2d6a301
Michał Segieta <segieta.michal@gmail.com>
2017-06-07 23:19:44 +0200

    selection methods

--- GeneAlgo/Main.cs
+++ GeneAlgo/Main.cs
@@ -23,10 +23,13 @@ namespace GeneAlgo
             return -(20 + (Math.Pow(x, 2) - 20 * Math.Cos(2 * Math.PI * Math.Pow(x, 2))) + (Math.Pow(y, 2) - 20 * Math.Cos(2 * Math.PI * Math.Pow(y, 2))));
         }
 
+
+        List<Person> pop = Person.BornPop();
+        List<Person> newborn = new List<Person>();
+    
         List<Person> CalcFunctValue()
         {
-            List<Person> pop = Person.BornPop();
-            List<Person> newborn = new List<Person>();
+            
             foreach (var item in pop)
             {
                 item.getPersonZ = RastriginFunction(item.getPersonX, item.getPersonY);
@@ -36,5 +39,34 @@ namespace GeneAlgo
             return newborn;
         }
 
+        List<Person> selectedpop = new List<Person>();
+        List<Person> SelectionByAverage() //jedna metoda selekcji
+        {
+            var average = newborn.Average(item => item.getPersonZ);
+
+            var MakeSelectionByAverage = from q in newborn
+                                         where q.getPersonZ > average
+                                         select q;
+
+            foreach (var q in MakeSelectionByAverage)
+            {
+                selectedpop.Add(q);
+            }
+            return selectedpop;
+        }
+
+        List<Person> SelectionByBest() //druga metoda selekcji
+        {
+
+
+            var MakeSelectionByBest = (from q in newborn
+                                       select q).Take(2);
+
+            foreach (var q in MakeSelectionByBest)
+            {
+                selectedpop.Add(q);
+            }
+            return selectedpop;
+        }
     }
 }
